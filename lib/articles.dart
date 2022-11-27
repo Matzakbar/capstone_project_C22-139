ed1b45f60849113d9f48729d1606aa05774d0b7d
Matzakbar <mattakbar02@gmail.com>
2022-11-27 12:17:52 +0700

    tes

---
+++ lib/articles.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+class Article {
+  final String title;
+  final String description;
+  final String urlToImage;
+
+  Article({
+    required this.title,
+    required this.description,
+    required this.urlToImage,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> article) => Article(
+        title: article['title'],
+        description: article['description'],
+        urlToImage: article['urlToImage'],
+      );
+}
+
+List<Article> parseArticles(String? json) {
+  if (json == null) {
+    return [];
+  }
+
+  final List parsed = jsonDecode(json);
+  return parsed.map((json) => Article.fromJson(json)).toList();
+}
--- lib/pages/articles_page.dart
+++ lib/pages/articles_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:capstone_project/articles.dart';
 
 class ArticlesPage extends StatelessWidget {
   const ArticlesPage({Key? key}) : super(key: key);
@@ -7,15 +8,42 @@ class ArticlesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Articles',
+        appBar: AppBar(
+          backgroundColor: Colors.brown.shade800,
+          title: const Text(
+            'Articles',
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => ZoomDrawer.of(context)!.toggle(),
+          ),
         ),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () => ZoomDrawer.of(context)!.toggle(),
-        ),
-      ),
-    );
+        body: FutureBuilder<String>(
+          future: DefaultAssetBundle.of(context)
+              .loadString('assets/articles/articles.json'),
+          builder: (context, snapshot) {
+            final List<Article> articles = parseArticles(snapshot.data);
+            return ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                return BuildArticleItem(context, articles[index]);
+              },
+            );
+          },
+        ));
   }
+}
+
+// ignore: unused_element, non_constant_identifier_names
+Widget BuildArticleItem(BuildContext context, Article article) {
+  return ListTile(
+    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    leading: Image.network(
+      article.urlToImage,
+      width: 100,
+    ),
+    title: Text(article.title),
+    subtitle: Text(article.description),
+    onTap: () {},
+  );
 }
